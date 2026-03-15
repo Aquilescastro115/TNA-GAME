f8c2b3408ff2976c8f9729f461d448f5db12dbae
Aquilescastro115 <aquilescastrosas@gmail.com>
2026-03-14 21:55:25 -0300

    sas

---
+++ Scripts/Ant/Ant.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+enum State {SEARCH,RETURN_HOME}
+
+@export var speed = 40
+
+var state = State.SEARCH
+var direction = Vector2.ZERO
+var nest_position = Vector2(80, 128)
+
+func _ready():
+	randomize()
+	change_direction()
+
+func _physics_process(delta):
+
+	match state:
+
+		State.SEARCH:
+			search_food()
+
+		State.RETURN_HOME:
+			go_home()
+
+	velocity = direction * speed
+	move_and_slide()
+
+	if velocity.length() > 0:
+		rotation = velocity.angle()
+
+func search_food():
+
+	if randi() % 100 < 2:
+		change_direction()
+
+
+func go_home():
+
+	direction = (nest_position - global_position).normalized()
+
+	if global_position.distance_to(nest_position) < 10:
+		state = State.SEARCH
+
+
+func change_direction():
+
+	var angle = randf() * TAU
+	direction = Vector2(cos(angle), sin(angle))
+	
+	
